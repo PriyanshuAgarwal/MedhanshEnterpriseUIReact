a57a25280f892b6684a26a114e4e67f7ecd40557
PriyanshuAgarwal <agarwal.iet888@gmail.com>
2022-02-12 00:43:34 +0530

    Finish Contact US page UI

--- src/js/MedContactUs.js
+++ src/js/MedContactUs.js
@@ -2,11 +2,17 @@ import React from "react";
 import Header from './Header';
 import MedImageCarouselHome from './MedImageCarouselHome';
 import '../css/MedContactUs.css';
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css';
+import MedFooter from './MedFooter';
+import MedContactInfo from './MedContactInfo';
 
 
 
 
 class MedContactUs extends React.Component {
+
+
     render() {
         return (
             <div>
@@ -30,19 +36,38 @@ class MedContactUs extends React.Component {
                                         <font>Please include product name, order quantity, usage, special requests if any in your inquiry.</font>
                                     </font>
                                 </p>
-                                <textarea name="message" id="message" spellcheck="false" data-gramm="false"></textarea>
+                                <textarea name="message" id="message" spellCheck="false" data-gramm="false"></textarea>
                             </dd>
-                            <dd className="mr-2">
-                                <label><font ><font >Email </font></font><span><font ><font >*</font></font></span></label>
-                                <input placeholder="Enter your email" type="email" name="email" class="validate[required,custom[email]] valid" id="email" value=""></input>
-                             </dd>
-                             <dd id="mobile-dd">
-                                <label><font><font>Mobile no. </font></font><span><font><font>*</font></font></span></label>
-                                <input id="phone" type="tel" name="phone"></input>
+                            <div className="med-contact-us-details">
+                                <dd className="mr-2">
+                                    <label><font ><font >Email </font></font><span><font ><font >*</font></font></span></label>
+                                    <input placeholder="Enter your email" type="email" name="email" className="validate[required,custom[email]] valid" id="email" value=""></input>
+                                </dd>
+                                <dd className="med-mobile-details">
+                                    <label><font ><font >Mobile No </font></font><span><font ><font >*</font></font></span></label>
+                                    <PhoneInput
+                                        inputProps={{
+                                            name: 'phone',
+                                            required: true,
+                                            autoFocus: true
+                                        }}
+                                        country="in"
+                                        placeholder="Enter your Mobile Number"
+                                        />
+                                </dd>
+                            </div>
+                            <dd className="dd-100 center submit-btn-container">
+                                <font><font>
+                                    <input value="Send Inquiry" className="btn yellow-btn" name="Submit" type="submit"></input>
+                                </font></font>
                             </dd>
+                            <div className="bulk-mssg-container">
+                                <strong><font><font>“We are dealing in bulk order quantity”</font></font><br></br></strong></div>
                         </div>
                     </div>
                 </div>
+                <MedContactInfo></MedContactInfo>
+                <MedFooter></MedFooter>
             </div>
 
         
