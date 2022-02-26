ee9915c56ceb00237037724ba8953cb2287c539e
PriyanshuAgarwal <agarwal.iet888@gmail.com>
2022-02-26 19:05:45 +0530

    Fix menubar for small scrrens

--- src/js/MedMenubar.js
+++ src/js/MedMenubar.js
@@ -3,11 +3,48 @@ import { Link } from "react-router-dom";
 import '../css/MedMenubar.css';
 
 class MedMenubar extends React.Component {
+	componentDidMount() {
+		document.addEventListener("DOMContentLoaded", function(){
+			// make it as accordion for smaller screens
+			if (window.innerWidth < 992) {
+			
+			  // close all inner dropdowns when parent is closed
+			  document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
+				everydropdown.addEventListener('hidden.bs.dropdown', function () {
+				  // after dropdown is hidden, then find all submenus
+					this.querySelectorAll('.submenu').forEach(function(everysubmenu){
+					  // hide every submenu as well
+					  everysubmenu.style.display = 'none';
+					});
+				})
+			  });
+			
+			  document.querySelectorAll('.dropdown-menu a').forEach(function(element){
+				element.addEventListener('click', function (e) {
+					let nextEl = this.nextElementSibling;
+					if(nextEl && nextEl.classList.contains('submenu')) {	
+					  // prevent opening link if link needs to open dropdown
+					  e.preventDefault();
+					  if(nextEl.style.display == 'block'){
+						nextEl.style.display = 'none';
+					  } else {
+						nextEl.style.display = 'block';
+					  }
+			
+					}
+				});
+			  })
+			}
+			// end if innerWidth
+			});
+	}
+
 	render() {
 		return (
 			<div className="med-menubar">
 				<div className="container">
 				<nav className="navbar main-nav navbar-expand-lg">
+					<div class="navbar-brand">Menu</div>
 					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav">
 						<span className="navbar-toggler-icon"></span>
 					</button>
